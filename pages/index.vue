60d418db9071e50983fa57795885125aeeaa6631
Andre Monello <atmonello@gmail.com>
2019-03-16 11:54:16 -0300

    Adicionados arquivos do layout principal.

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <layout-main></layout-main>
+  <layout-main>
+    <container-lista slot="lista"></container-lista>
+  </layout-main>
 </template>
 
 <script>
-import layoutMain from "../layouts/main.vue";
+import layoutMain from "../layouts/mainLayout.vue";
+
+import containerLista from "../components/containerLista.vue";
 
 export default {
-  components: {
-    layoutMain
-  }
+    components: {
+        layoutMain,
+        containerLista,
+    }
 };
 </script>
