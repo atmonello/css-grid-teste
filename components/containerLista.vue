5f70767c3e5f81c91e7e0a7033b0a9644c25e632
Andre Monello <atmonello@gmail.com>
2019-03-16 16:08:51 -0300

    Implementacao de chamada do GraphQL ao entrar na página.

--- components/containerLista.vue
+++ components/containerLista.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="container-lista">
-    <item-lista></item-lista>
-    <item-lista></item-lista>
-    <item-lista></item-lista>
+    <item-lista v-for="(veiculo, index) in listaVeiculos" :key="index" :info-veiculo="veiculo.node"></item-lista>
   </div>
 </template>
 
 <style lang='less'>
+.container-lista {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  grid-gap: 8px;
+}
 </style>
 
 
@@ -16,6 +20,11 @@ import itemLista from "./itemLista.vue";
 export default {
     components: {
         itemLista
+    },
+    props: {
+        listaVeiculos: {
+            required: true
+        }
     },
     data() {
         return {
--- components/itemLista.vue
+++ components/itemLista.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="item-lista">
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit neque nisi non recusandae rerum delectus vero iste repellendus ratione pariatur atque, hic suscipit voluptatibus. Aspernatur nobis illo expedita fuga animi?</p>
+    <!-- <p>{{ infoVeiculo }}</p> -->
+    <p class="marca">{{ infoVeiculo.marca }}</p>
+    <p class="modelo">{{ infoVeiculo.modelo }}</p>
+    <p class="ano_modelo">{{ infoVeiculo.ano_modelo }}</p>
   </div>
 </template>
 
@@ -12,12 +15,38 @@
     border-radius: 16px;
     box-shadow: @shadow;
     width: 100%;
+    padding: 1em;
+
+    p {
+      margin: 0;
+      font-size: @ftSizeSmall;
+
+      &.marca {
+        color: @black;
+        text-transform: uppercase;
+      }
+
+      &.modelo {
+        color: @blue-md;
+      }
+
+      &.ano_modelo {
+        color: @gray-md;
+        font-size: @ftSizeTiny;
+      }
+    }
 }
 </style>
 
 
 <script>
 export default {
+    props: {
+        infoVeiculo: {
+            required: true,
+            type: Object
+        }
+    },
     data() {
         return {
         };
